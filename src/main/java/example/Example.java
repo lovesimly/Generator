2dfb91edf3601b6ea9538f948d0e606e24ac743b
lovesimly <lovesimly@126.com>
2017-05-03 18:24:27 +0800

    初始导入

---
+++ src/main/java/example/Example.java
@@ -0,0 +1,12 @@
+package example;
+
+/**
+ * Created by lujingzhong on 2017/5/3.
+ */
+public class Example {
+
+    public static void main(String args){
+        System.out.printf("sh cmd 命令");
+
+    }
+}
